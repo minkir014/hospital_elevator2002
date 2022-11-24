9d0229ed7e8a2715e2b7fcd7514ffbdc2e723c8c
Mina <mina.fawzy02@eng-st.cu.edu.eg>
2022-11-24 19:02:33 +0200

    Heap Destructor Added

--- Hospital_Elevator/Hospital_Elevator/LinkedHeap.h
+++ Hospital_Elevator/Hospital_Elevator/LinkedHeap.h
@@ -3,7 +3,7 @@
 #include "HeapNode.h"
 
 template <typename T>
-class LinkedHeap {
+class LinkedHeap : Heap<T> {
 private:
 	HeapNode<T>* root;
 	int count;
@@ -12,14 +12,15 @@ private:
 	HeapNode<T>* reHeapDown(HeapNode<T>* parent);
 	bool Rebuild(HeapNode<T>* SubTreeRoot);
 	HeapNode<T>* insert(HeapNode<T>* parent, HeapNode<T>* newNode, int determiner);
+
+	bool DestroyHeap(HeapNode<T>* SubTreeRoot);
 public:
 	LinkedHeap();
-	//~LinkedHeap();
+	~LinkedHeap();
 
 	bool Insert(const T& Value);
 
 	bool ReBuild();
-
 
 	bool Delete();
 
