495a6b5b21d66fbfd2622a73dba7a059d0d11192
mtheggi <mtheggi@gmail.com>
2022-11-30 21:37:50 +0200

    add allHeader.h [includes all .h files ]

---
+++ Hospital_Elevator/Hospital_Elevator/AllHeaders.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>; 
+#include <cmath>; 
+#include<thread > 
+#include "Floor.h"
+#include "Defs.h"
+#include "Event.h" 
+#include "Hospital.h" 
+#include "PickablePtr.h" 
+#include "Pickable.h"
+#include "UI.h" 
+#include "Elevator.h"
+#include "EventArrival.h"
+#include "Elevator.h" 
+#include "LinkedQueue.h"
+#include "LinkedHeap.h"
+#include"ArrayBag.h"
+#include "LinkedBag.h"
