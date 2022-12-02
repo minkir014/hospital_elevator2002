8d17f7fc993603fb0100e08f76267959f515ed4d
mtheggi <mtheggi@gmail.com>
2022-12-02 21:57:17 +0200

    UpdateState Function in Elevator

--- Hospital_Elevator/Hospital_Elevator/Elevator.cpp
+++ Hospital_Elevator/Hospital_Elevator/Elevator.cpp
@@ -2,6 +2,7 @@
 #include "Elevator.h"
 #include "Pickable.h"
 #include <string>
+#include "Defs.h"
 using namespace std;
 
 Elevator::Elevator(int CSpeed, int CCapacity):Elevatorhead(CCapacity)
@@ -9,7 +10,135 @@ Elevator::Elevator(int CSpeed, int CCapacity):Elevatorhead(CCapacity)
 	CurrentFloor = 0;
 	Speed = CSpeed;
 	Capacity = CCapacity;
+	state = AVAIL; 
+	LastState = AVAIL; 
+
 }
+
+
+void Elevator::incrementFloor() { CurrentFloor++; }
+void Elevator::decrementFloor() { CurrentFloor--;  }
+ElevatorState Elevator::getState() const { return state;  }
+int Elevator::getCurrentFloor() const { return CurrentFloor;  }
+int Elevator::getSpeed() const { return Speed;  }
+int Elevator::getTimeOfLastFloor() const { return timeOfLastFloor;  }
+int Elevator::getCapacity() const { return Capacity;}
+void Elevator::UpdateState(int& timestep , int &Floor ,int & maxFloor ) {
+	if (state == MovingDown) {
+		if ((timestep - timeOfLastFloor) % Speed == 0) {
+			decrementFloor(); 
+			timeOfLastFloor = timestep; 
+			if (CurrentFloor == 0 ) {
+				if(Elevatorhead.isEmpty()) {
+					state = IDLE; 					
+				}
+				else{
+					state = Unload; 
+				}
+			}
+			else if (CurrentFloor == Floor ) {
+				if (Elevatorhead.isEmpty()) {
+					state = AVAIL;
+				}
+				else {
+					state = Unload;
+				}
+			}
+			LastState = MovingDown;
+		}
+	}
+	if (state == MovingUp) {
+		if ((timestep - timeOfLastFloor) % Speed == 0) {
+			incrementFloor();
+			timeOfLastFloor = timestep;
+			if (CurrentFloor == maxFloor) {
+				if (Elevatorhead.isEmpty()) {
+					state = IDLE;
+				}
+				else {
+					state = Unload;
+				}
+			}
+			else if (CurrentFloor == Floor) {
+				if (Elevatorhead.isEmpty()) {
+					state = AVAIL;
+				}
+				else {
+					state = Unload;
+				}
+			}
+			LastState = MovingUp;
+		}
+
+	}
+	if(state == IDLE && (timestep - timeOfLastFloor) == 3) {
+		state = AVAIL;
+		timeOfLastFloor = timestep;
+		LastState = IDLE;
+	}
+	if(state == AVAIL && (timestep - timeOfLastFloor) == 10) {
+		state = IDLE; 
+		timeOfLastFloor = timestep; 
+		LastState = AVAIL; 
+	}
+	if (state == AVAIL) {
+		if (Floor > CurrentFloor ) {
+			if (LastState == MovingDown) {
+				state = IDLE;
+				timeOfLastFloor = timestep -1 ;
+			}
+			else{
+				state = MovingUp;
+				timeOfLastFloor = timestep;
+			}
+			LastState = AVAIL; 
+		}
+		else if (Floor < CurrentFloor) {
+			if (LastState == MovingUp) {
+				state = IDLE; 			
+				timeOfLastFloor = timestep - 1;
+			}
+			else {
+				state = MovingDown; 	
+				timeOfLastFloor = timestep;
+			}
+			LastState = AVAIL; 
+		}
+	}
+	if (state == Unload) {
+		if (LastState == MovingDown) {
+			if ((Floor - CurrentFloor) > 0) {
+				state = IDLE; 
+			}
+			else if ((Floor - CurrentFloor) < 0) {
+				state = MovingDown; 
+			}
+			else {
+				state = AVAIL; 
+			}
+		}
+		else if( LastState == MovingUp) {
+				if ((Floor - CurrentFloor) < 0) {
+					state = IDLE;
+				}
+				else if ((Floor - CurrentFloor) > 0) {
+					state = MovingUp;
+				}
+				else {
+					state = AVAIL;
+				}
+		}
+		timeOfLastFloor = timestep; 
+		LastState = Unload; 
+	}
+	/* In bonus 
+	if(state == OUT) {
+	//	timeOfLastFloor = 			
+	}
+	*/
+}
+
+
 
 Elevator::~Elevator()
 {
--- Hospital_Elevator/Hospital_Elevator/Elevator.h
+++ Hospital_Elevator/Hospital_Elevator/Elevator.h
@@ -2,6 +2,7 @@
 #include "ArrayBag.h"
 #include "Pickable.h"
 #include <string>
+#include "Defs.h"
 using namespace std;
 
 class Elevator
@@ -11,9 +12,20 @@ private:
 	int timeOfLastFloor;    // The timestep it was at
 	int Speed;				//Time steps needed to move up or down a Floor
 	int Capacity;			//max capicity of the elevator 
+	ElevatorState state; 
+	ElevatorState LastState; 
 	ArrayBag<Pickable*> Elevatorhead;	//bag containig People in the elevator
 public:
 	Elevator(int CSpeed, int CCapacity);
+	void incrementFloor();
+	void decrementFloor();
+	ElevatorState getState() const; 
+	int getCurrentFloor() const; 
+	int getSpeed() const; 
+	int getTimeOfLastFloor() const;
+	void UpdateState( int & timestep, int &Floor , int &maxFloor);
+	int getCapacity() const; 
+
 	~Elevator();
 
 };
